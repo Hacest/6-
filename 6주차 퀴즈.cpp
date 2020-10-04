b96f6a6e59e5ec97ae2c2e8361636330a8d1383b
Hacest <71202550+Hacest@users.noreply.github.com>
2020-10-04 21:00:45 +0900

    Add files via upload

---
+++ 6주차 퀴즈.cpp
@@ -0,0 +1,19 @@
+﻿#include <bangtal.h>
+
+using namespace std;
+using namespace bangtal;
+
+int main()
+{
+	class Door: public Object
+	{
+	public:
+		bool closed = true;
+	};
+
+	class LockedDoor: public Door
+	{
+	public:
+		bool locked = true;
+	};
+}
